35fe22d1d20318c80d4f220dc8cd7ab3e277bd4b
Stephani Knupp <stephaniknupp@hotmail.com>
2026-08-21 22:20:33 -0300

    cria model solve

---
+++ src/models/solve.ts
@@ -0,0 +1,34 @@
+export class Solve {
+    private _id: string;
+    private _userId: string;
+    private _time: number;
+    private _createdAt: Date;
+
+
+    constructor(id: string, userId: string, time: number, createdAt: Date) {
+        this._id = id;
+        this._userId = userId;
+        this._time = time;
+        this._createdAt = createdAt;
+    }
+
+    
+    get id(): string {
+        return this._id;
+    }
+
+
+    get userId(): string {
+        return this._userId;
+    }
+
+    
+    get time(): number {
+        return this._time;
+    }
+
+
+    get createdAt(): Date {
+        return this._createdAt;
+    }
+}
